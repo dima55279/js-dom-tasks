be3cf264bfe79febea4b074fa7727f97a99058f0
dima55279 <dima55279@yandex.ru>
2025-05-13 10:09:13 +0300

    Выполнено задание 4.

--- solutions/4-dom-hierarchy.js
+++ solutions/4-dom-hierarchy.js
@@ -1,3 +1,11 @@
 // BEGIN
-
+const extractData = (DOM) => {
+    const body = DOM.querySelector('body');
+    const list = Array.from(body.childNodes);
+    const paragraphs = list.filter((el) => el.tagName === 'p' || el.tagName === 'P');
+    const data = [];
+    paragraphs.forEach(paragraph => data.push(paragraph.textContent.trim()));
+    return data;
+}
+export default extractData;
 // END
