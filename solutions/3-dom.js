0ce16d21be0349e59ab99f3127659df61623bf6e
dima55279 <dima55279@yandex.ru>
2025-05-13 09:51:09 +0300

    Выполнено задание 3.

--- solutions/3-dom.js
+++ solutions/3-dom.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+let body = document.body.innerHTML;
+let newBody = "";
+while (body.includes("."))
+{
+    newBody += '<p>' + body.slice(0, body.indexOf(".") + 1).trim() + '</p>';
+    body = body.slice(body.indexOf(".") + 1);
+}
+document.body.innerHTML = newBody;
 // END
