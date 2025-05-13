143f4c6ffcc765531ecba0274047b8ded17d767f
dima55279 <dima55279@yandex.ru>
2025-05-13 13:49:01 +0300

    Выполнено задание 5.

--- solutions/5-search.js
+++ solutions/5-search.js
@@ -1,5 +1,16 @@
 export default (document) => {
   // BEGIN
-
+    const data = {};
+    const page = document.querySelector('.content');
+    data.title = page.querySelector('h1').textContent;
+    data.description = page.querySelector('.description').textContent;
+    data.items = []
+    const linksChildren = page.querySelectorAll('.links > div');
+    linksChildren.forEach(item => {
+        const title = item.querySelector('a').textContent;
+        const description = item.querySelector('p').textContent;
+        data.items.push({title: title, description: description});
+    })
+    return data;
   // END
 };
