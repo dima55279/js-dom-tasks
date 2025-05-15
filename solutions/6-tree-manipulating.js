7707e321942cafc6a7df2dc595a6b51483f578ec
dima55279 <dima55279@yandex.ru>
2025-05-15 20:51:17 +0300

    Выполнено задание 6.

--- solutions/6-tree-manipulating.js
+++ solutions/6-tree-manipulating.js
@@ -1,3 +1,22 @@
 // BEGIN
-
+const prettify = (document) => {
+    const divs = Array.from(document.getElementsByTagName('div'));
+    divs.forEach((div) => {
+        const children = div.childNodes;
+        children.forEach((child) => {
+            if (child instanceof Text)
+            {
+                if (child.textContent.includes("\n") === false)
+                {
+                    const text = child.textContent;
+                    const p = document.createElement('p');
+                    p.textContent = text;
+                    child.replaceWith(p);
+                }
+            }
+        })
+    })
+    return document;
+}
+export default prettify;
 // END
