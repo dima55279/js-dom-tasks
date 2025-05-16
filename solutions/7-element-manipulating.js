12f5dcf49021b1ee77eb86801e8880b9a0122870
dima55279 <dima55279@yandex.ru>
2025-05-16 15:02:28 +0300

    Выполнено задание 7.

--- solutions/7-element-manipulating.js
+++ solutions/7-element-manipulating.js
@@ -1,5 +1,15 @@
 import camelCase from 'lodash/camelCase';
 
 // BEGIN
-
+const normalize = (document) => {
+    const elements = Array.from(document.body.getElementsByTagName('*'));
+    elements.forEach(element => {
+        const classList = element.classList;
+        classList.forEach(oneClass => {
+            classList.replace(oneClass, camelCase(oneClass));
+        })
+    })
+    return document;
+}
+export default normalize;
 // END
