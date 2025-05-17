3f0cbe8ae5c051457261b665a160721d4ca8dbbe
dima55279 <dima55279@yandex.ru>
2025-05-17 18:32:55 +0300

    Выполнено задание 8.

--- solutions/8-events.js
+++ solutions/8-events.js
@@ -1,5 +1,14 @@
 export default () => {
   // BEGIN
-
+    const button = document.getElementById('alert-generator');
+    const mainDiv = document.querySelector('.alerts');
+    let counter = 1;
+    button.addEventListener('click', () => {
+        const div = document.createElement('div');
+        div.classList.add('alert', 'alert-primary');
+        div.textContent = "Alert " + String(counter);
+        mainDiv.insertBefore(div, mainDiv.firstChild);
+        counter++;
+    })
   // END
 };
