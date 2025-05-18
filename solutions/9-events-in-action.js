75695db29fdbe7bd9174f16a4e4d93027eb9ccda
dima55279 <dima55279@yandex.ru>
2025-05-18 20:34:07 +0300

    Выполнено задание 9.

--- solutions/9-events-in-action.js
+++ solutions/9-events-in-action.js
@@ -1,5 +1,22 @@
 export default () => {
   // BEGIN
-  
+  const buttons = document.querySelectorAll('[data-bs-toggle]');
+  buttons.forEach(button => {
+      button.addEventListener('click', () => {
+          if (button.classList.contains('active') === false) {
+              const activeElements = document.querySelectorAll('.active');
+              activeElements.forEach(activeElement => {
+                  if (activeElement.dataset.bsToggle === button.dataset.bsToggle) {
+                      activeElement.classList.remove('active');
+                      const sameIDActiveElement = document.querySelector(activeElement.dataset.bsTarget);
+                      sameIDActiveElement.classList.remove('active');
+                  }
+              })
+              button.classList.add('active');
+              const sameIDDiv = document.querySelector(button.dataset.bsTarget);
+              sameIDDiv.classList.add('active');
+          }
+      })
+  })
   // END
 };
