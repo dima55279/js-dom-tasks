5f6ace8d80c808200f4326cd8a3d3aea7e0d79ab
dima55279 <dima55279@yandex.ru>
2025-05-26 23:23:04 +0300

    Выполнено задание 10.

--- solutions/10/src/application.js
+++ solutions/10/src/application.js
@@ -1,5 +1,25 @@
 import escapeHtml from 'escape-html';
 
 // BEGIN
-
+const formHandler = () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const name = formData.get('name');
+        const comment = formData.get('comment');
+        const newBlock = document.createElement('div');
+        newBlock.appendChild(document.createElement('p'));
+        newBlock.lastChild.textContent = 'Feedback has been sent';
+        newBlock.appendChild(document.createElement('div'));
+        newBlock.lastChild.textContent = 'Email: ' + email;
+        newBlock.appendChild(document.createElement('div'));
+        newBlock.lastChild.textContent = 'Name: ' + name;
+        newBlock.appendChild(document.createElement('div'));
+        newBlock.lastChild.textContent = 'Comment: ' + comment;
+        form.replaceWith(newBlock);
+    });
+}
+export default formHandler;
 // END
