ba59ad51a33b06ec1af5931765b471228e4711b1
dima55279 <dima55279@yandex.ru>
2025-05-27 00:10:18 +0300

    Выполнено задание 11. Закончена ЛР 8.

--- solutions/11/src/application.js
+++ solutions/11/src/application.js
@@ -2,6 +2,28 @@ import 'whatwg-fetch';
 
 export default () => {
   // BEGIN
-  
+  const host = window.location.origin;
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(input => {
+      input.addEventListener('input', async (e) => {
+          const autocomplete = input.dataset.autocompleteName;
+          const api = input.dataset.autocomplete;
+          const list = document.querySelector('ul[data-autocomplete-name=' + autocomplete + ']')
+          list.innerHTML = '';
+          const url = new URL(api, host);
+          url.searchParams.set('search', e.target.value);
+          const response = await fetch(url);
+          const data = await response.json();
+          if (data.length > 0) {
+              data.forEach(item => {
+                  const li = document.createElement('li');
+                  li.textContent = item;
+                  list.append(li);
+              });
+          } else {
+              list.innerHTML = '<li>Nothing</li>';
+          }
+      })
+  })
   // END
 };
